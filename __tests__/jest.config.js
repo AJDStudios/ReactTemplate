96a37e3bce898e333e96ae9224237159425f5bae
AJDStudios <aaronjdodd1989@mail.ru>
2023-05-25 20:17:31 +0100

    init jest

---
+++ __tests__/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleFileExtensions: ['js', 'jsx'],
+  testMatch: ['<rootDir>/src/**/*.test.{js,jsx}'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/setupTests.js'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}', '!<rootDir>/src/index.js'],
+  coverageReporters: ['text', 'lcov'],
+};
