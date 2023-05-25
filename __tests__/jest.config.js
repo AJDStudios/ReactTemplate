4e8ee017ac22f6b19a285b9439795db06f1e49a3
AJDStudios <aaronjdodd1989@mail.ru>
2023-05-25 20:35:12 +0100

    added readme and included a basic test + pther small fixes

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -1,15 +1,27 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  moduleFileExtensions: ['js', 'jsx'],
-  testMatch: ['<rootDir>/src/**/*.test.{js,jsx}'],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  moduleFileExtensions: ["js", "jsx"],
+  testMatch: ["<rootDir>/src/**/*.test.{js,jsx}"],
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
+    "\\.(css|less|scss)$": "identity-obj-proxy",
   },
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/setupTests.js'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}', '!<rootDir>/src/index.js'],
-  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/src/setupTests.js",
+  ],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{js,jsx}",
+    "!<rootDir>/src/index.js",
+  ],
+  coverageReporters: ["text", "lcov"],
 };
+
+// myTest.test.js
+
+test("example test", () => {
+  expect(1 + 1).toBe(2);
+});
