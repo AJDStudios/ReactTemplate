c65cfeffea9724b9beabfac980ea79e36221e76f
AJDDevelopment <aarondodd@mail.ru>
2023-05-28 19:47:40 +0100

    directory restructure and rooting functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header.js';
 // import Footer from "./components/Footer";
-// import About from "./pages/About";
-// import Skills from "./pages/Skills";
-// import Content from "./pages/Content";
-// import Contact from "./pages/Contact";
-import SplashScreen from './SplashScreen';
-import './App.css';
+import Content from "./pages/Content.js";
+import MoreContent from "./pages/MoreContent.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
+import SplashScreen from './pages/SplashScreen.js';
+import './styles/App.css';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true); // Set this flag to indicate if the app is still loading or ready
@@ -26,11 +26,10 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<SplashScreen />} />
-            {/* Add other routes for your template pages */}
-            {/* <Route path="/About" element={<About />} />
+            <Route path="/Content" element={<Content />} /> 
             <Route path="/MoreContent" element={<MoreContent />} />
-            <Route path="/Content" element={<Content />} />
-            <Route path="/Contact" element={<Contact />} /> */}
+            <Route path="/About" element={<About />} />
+            <Route path="/Contact" element={<Contact />} />
           </Routes>
           {/* <Footer /> */}
         </Router>
---
+++ src/pages/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div>
+      <h2>Content Page</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at mi nec purus tempor fringilla sed id lectus. Vivamus id mauris euismod, posuere felis sed, malesuada urna. Aliquam suscipit efficitur arcu, vel eleifend urna consequat a.</p>
+    </div>
+  );
+};
+
+export default Content;
