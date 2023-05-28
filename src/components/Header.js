44b9d3c8a93843960b38df778255337d52d81c4c
AJDDevelopment <aarondodd@mail.ru>
2023-05-28 11:21:04 +0100

    mobile nav behaviour

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,23 @@ import { Link } from 'react-router-dom';
 import { Navigation } from './Nav.js';
 import DeskNavbar from "./DeskNav.js";
 import logo from "../logo.webp";
+import '../styles/Header.css';
 
 
 function Header() {
   return (
-    <nav className="header">
-      <Link to="/" className="site-logo">
-        <img src={logo} alt="Logo" />
-      </Link>
-      
+    <header className="header">
+        <div className="container">
+            <Link to="/" className="site-logo">
+                <img src={logo} alt="Logo" />
+            </Link>
+            
 
-      <Navigation />
+            <Navigation />
 
-      <DeskNavbar />
-    </nav>
+            <DeskNavbar />
+        </div>
+    </header>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,52 +1,35 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Nav.css';
 
 function CustomLink({ to, children }) {
-    return <Link to={to}>{children}</Link>;
+  return <Link to={to}>{children}</Link>;
 }
 
 function Navigation() {
     const [showMenu, setShowMenu] = useState(false);
   
-    let theMenu;
-    let menuMask;
-  
-    if (showMenu) {
-      theMenu = (
-        <div className="menu">
-          <ul className="nav-links">
-            <CustomLink to="/content">Content</CustomLink>
-            <CustomLink to="/About">About</CustomLink>
-            <CustomLink to="/OtherContent">OtherContent</CustomLink>
-            <CustomLink to="/Contact">Contact</CustomLink>
-          </ul>
-        </div>
-      );
-  
-      menuMask = (
-        <div
-          className="menumask"
-          onClick={() => setShowMenu(false)}
-        ></div>
-      );
-    }
-  
     return (
       <nav className="nav">
         <span
-          className="burger"
+          className={`burger ${showMenu ? 'open' : ''}`}
           onClick={() => setShowMenu(!showMenu)}
         >
-          ☰
+          {showMenu ? '✕' : '☰'}
         </span>
   
-        {menuMask}
-  
-        {theMenu}
+        {showMenu && (
+          <div className="menu">
+            <ul className="nav-links">
+              <CustomLink to="/content">Content</CustomLink>
+              <CustomLink to="/OtherContent">OtherContent</CustomLink>
+              <CustomLink to="/About">About</CustomLink>
+              <CustomLink to="/Contact">Contact</CustomLink>
+            </ul>
+          </div>
+        )}
       </nav>
     );
   }
   
-  export { CustomLink, Navigation };
-
-  
+export { CustomLink, Navigation };
