8c06035f90bca228c4ee7a90da91db06081a088f
AJDDevelopment <aarondodd@mail.ru>
2023-05-28 17:42:06 +0100

    finished battling with testing.

--- src/__tests__/Nav.test.jsx
+++ src/__tests__/Nav.test.jsx
@@ -1,7 +1,65 @@
-describe('Math', () => {
-    test('performs a logic-based math calculation', () => {
-      const result = 2 + 2 * 2; // Perform a logic-based math calculation
-      expect(result).toEqual(6); // Assert the expected result
+import { MemoryRouter } from 'react-router-dom';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Navigation } from '../components/Nav';
+import { createMemoryHistory } from 'history';
+
+
+//test for the presense of the burger icon
+describe('Navigation', () => {
+  const originalWindow = { ...window };
+
+  beforeEach(() => {
+    delete window.innerWidth;
+    window.innerWidth = 0;
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      value: 0,
+    });
+    window.dispatchEvent = jest.fn();
+  });
+
+  afterEach(() => {
+    Object.assign(window, originalWindow);
+  });
+
+  test('displays burger icon on mobile', () => {
+    // Set the window.innerWidth to simulate a mobile screen
+    window.innerWidth = 500;
+    window.dispatchEvent(new Event('resize'));
+
+    render(<Navigation />);
+    screen.debug();
+
+    const burgerIconsMobile = screen.queryAllByText('☰');
+    expect(burgerIconsMobile.length).toBe(1); // Expect burger icon to be displayed on mobile
+  });
+});
+
+describe('Navigation', () => {
+   test('should switch burger icon on click', () => {
+      const history = createMemoryHistory();
+      render(
+        <MemoryRouter history={history}>
+          <Navigation />
+        </MemoryRouter>
+      );
+        // Arrange
+        const burgerIcon = screen.getByText('☰');
+
+        // Assert initial state
+        expect(burgerIcon).toBeInTheDocument();
+    
+        // Act
+        fireEvent.click(burgerIcon);
+    
+        // Assert
+        expect(burgerIcon).toHaveTextContent('✕');
+    
+        // Act again
+        fireEvent.click(burgerIcon);
+    
+        // Assert
+        expect(burgerIcon).toHaveTextContent('☰');
     });
   });
-  
