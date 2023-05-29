af71052f8d9594ba9fb279414962d6f9c1f84e1e
AJDDevelopment <aarondodd@mail.ru>
2023-05-29 12:57:18 +0100

    added settings wheel with placeholder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { Navigation } from './Nav.js';
 import DeskNavbar from "./DeskNav.js";
 import logo from "../logo.webp";
 import '../styles/Header.css';
+import { FaCog } from 'react-icons/fa';
 
 
 function Header() {
+  const [showSettings, setShowSettings] = useState(false);
+
+function handleSettingsToggle() {
+  setShowSettings(!showSettings);
+}
+
   return (
     <header className="header">
         <div className="container">
@@ -19,7 +26,26 @@ function Header() {
             <Navigation />
 
             <DeskNavbar />
-        </div>
+
+            <div className="settings">
+              <FaCog className="settings-icon" onClick={handleSettingsToggle} />
+              {showSettings && (
+                <div className="settings-dropdown">
+                  <ul>
+                    <li>Theme: Light/Dark</li>
+                    <li>Language: English</li>
+                    <li>Language: French</li>
+                    <li>Language: German</li>
+                    <li>Language: Spanish</li>
+                    <li>Language: Czech</li>
+                    <li>Language: Russian</li>
+                    <li>Language: Welsh</li>
+                    <li>Language: Latin</li>
+                  </ul>
+                </div>
+              )}
+            </div>
+        </div>            
     </header>
   );
 }
